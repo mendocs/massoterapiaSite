2063835fd3ec6afc7123528b3701be2fc229f9c4
Ulisses Mendonca Junior <mendocs@gmail.com>
2021-03-11 01:14:42 -0300

    test routing server

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { MainContainerComponent } from './public-site/main-container/main-contai
 
 const routes: Routes = [
   {path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
+  {path: 'teste', component: MainContainerComponent},
   {path: '', component: MainContainerComponent}
 
 ];
